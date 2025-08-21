ea7abd7c513c2202b652d7d982122ce98d8c9099
osama_abdellatif <155384353+MAD-SAM22@users.noreply.github.com>
2025-08-22 01:09:11 +0300

    fixing Header import and JSX issues in Examples.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from './components/header.jsx';
+import Header from './components/Header.jsx';
 import CoreConcepts from './components/CoreConcepts.jsx';
 import Examples from './components/Examples.jsx';
 function App() {
